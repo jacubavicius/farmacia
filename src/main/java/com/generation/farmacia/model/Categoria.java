f777ba986afdd509e52f1cc2045d293048b341c4
gabriella_jacubavicius@hotmail.com <gabriella_jacubavicius@hotmail.com>
2024-07-22 15:56:54 -0300

    feat: CRUD Produto (6 métodos) com relacionamento com categoria

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Categoria {
 	@NotBlank(message = "O campo 'subCategoria' é obrigatório!")
 	@Size(min = 5, max = 50, message = "O campo 'subCategoria' deve ter no mínimo 5 e no máxico 50 caracteres")
 	private String subCategoria;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 	
 	public Long getId() {
 		return id;
@@ -47,4 +58,12 @@ public class Categoria {
 	public void setSubCategoria(String subCategoria) {
 		this.subCategoria = subCategoria;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}	
 }
